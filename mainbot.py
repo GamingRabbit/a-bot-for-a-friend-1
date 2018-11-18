a0b010a69235e6d397dd4c76b348d62eb5994521
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-11-18 12:17:15 +0100

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -3,3 +3,24 @@ from discord.ext.commands import Bot #not needed
 import discord
 from discord.ext import commands
 import random
+
+BOT_PREFIX=("cat!","msg!cat.")
+
+client = Bot(command_prefix=BOT_PREFIX)
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="msg!cat.help"))
+    print("Logged in as " + client.user.name)
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+ 
+
+
+
+client.loop.create_task(list_servers())
+client.run(os.getenv("TOKEN"))
